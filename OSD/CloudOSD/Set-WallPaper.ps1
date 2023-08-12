8810992d506f21eeab7d108ef4b938d7c0ce1911
XNDR/Freakthis <97555373+Freakthis@users.noreply.github.com>
2023-08-12 05:18:23 -0500

    Update Set-WallPaper.ps1
    Updated to personal wallpaper

--- OSD/CloudOSD/Set-WallPaper.ps1
+++ OSD/CloudOSD/Set-WallPaper.ps1
@@ -179,7 +179,7 @@ foreach ($filechild in $files)
 
 
 #Download WallPaper from GitHub
-$WallPaperURL = "https://github.com/gwblok/garytown/raw/master/OSD/CloudOSD/wallpaper.jpg"
+$WallPaperURL = "https://github.com/Freakthis/garytown/raw/master/OSD/CloudOSD/VDL_Wallpaper.jpg"
 Invoke-WebRequest -UseBasicParsing -Uri $WallPaperURL -OutFile "$env:TEMP\wallpaper.jpg"
 
 #Copy the 2 files into place
